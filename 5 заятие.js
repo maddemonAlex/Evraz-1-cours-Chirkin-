8d1d8f577a59459c040b979d2c42e21254e7d024
maddemonAlex <chirkin775@gmail.com>
2024-10-28 15:55:42 +0300

    5 занятие

---
+++ 5 заятие.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div style="font-size: 50px;">
+    Ответ: <span id="answer"></span>
+</div>
+
+<script>
+    let a = 1;
+    let b = "sadasdfs";
+    let c = true;
+    let d = null;
+    let t = a;
+    let r = a + t;
+    console.log(r);
+    console.log(r, a, t, d);
+</script>
+</body>
+</html>
