0d6134d29e5a946fa76032e983385efc3e4304c3
maddemonAlex <chirkin775@gmail.com>
2024-11-27 17:57:52 +0300

    11 занятие

--- 10 занятие.js
+++ 10 занятие.js
@@ -1,5 +1,33 @@
 function makeRed() {
-    let name=prompt('Укажите текст')
-    console.log(name);
-    let body = document.getElementById('name');
-    body.innerText='name'
+        let span = document.getElementById('span1');
+        span.innerText = '';
+        let span1 = document.getElementById('span2');
+        span1.innerText = '';
+        let span2 = document.getElementById('span3');
+        span2.innerText = '';
+}
+function make() {
+        let input = document.getElementById('d1');
+        let span = document.getElementById('span1');
+        span.innerText = span.innerText + input.value;
+        input.value = ''
+}
+function makef() {
+        let input = document.getElementById('d2');
+        let span = document.getElementById('span2');
+        span.innerText = span.innerText + input.value;
+        input.value = ''
+}
+function makeg() {
+        let input = document.getElementById('d3');
+        let span = document.getElementById('span3');
+        span.innerText = span.innerText + input.value;
+        input.value = ''
+}
+//
+// let a=1
+// console.log(a)
+// a=a+1
+// console.log(a)
+// a=a+1
+// console.log(a)
