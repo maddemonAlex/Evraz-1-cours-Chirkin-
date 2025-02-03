1e447b052d50184cc63b3108a5c1488b920c75c8
maddemonAlex <chirkin775@gmail.com>
2025-02-03 15:57:41 +0300

    верстка

--- занятие 2025 01 27.js
+++ занятие 2025 01 27.js
@@ -1,8 +1,33 @@
-let i = 4;
-
 function make() {
     let input = document.getElementById('d1');
-    let span = document.getElementById('span1');
-    span.innerText = span.innerText + input.value;
+    let liElement = document.createElement('li');
+    let addElement = document.getElementById('li1');
+    liElement.innerText = input.value;
+    addElement.append(liElement);
+    input.value = ''
+}
+
+function make2() {
+    let input = document.getElementById('d2');
+    let liElement = document.createElement('li');
+    let addElement = document.getElementById('li2');
+    liElement.innerText = input.value;
+    addElement.append(liElement);
+    input.value = ''
+}
+function ler(){
+    let input = document.getElementById('d3');
+    let liElement = document.createElement('li');
+    let addElement = document.getElementById('li1');
+    liElement.innerText = input.value;
+    addElement.append(liElement);
+    input.value = ''
+}
+function len(){
+    let input = document.getElementById('d4');
+    let liElement = document.createElement('li');
+    let addElement = document.getElementById('li2');
+    liElement.innerText * input.value;
+    addElement.append(liElement);
     input.value = ''
 }
