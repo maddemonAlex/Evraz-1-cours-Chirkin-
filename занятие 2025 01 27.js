28c7d54a8ec15e677e766979524897ba0dacf819
maddemonAlex <chirkin775@gmail.com>
2025-02-12 18:01:05 +0300

    верстка

--- занятие 2025 01 27.js
+++ занятие 2025 01 27.js
@@ -1,36 +1,30 @@
-function make() {
-    let input = document.getElementById('d1');
-    let liElement = document.createElement('li');
-    let addElement = document.getElementById('li1');
-
-    liElement.innerText = input.value;
-    addElement.append(liElement);
-    input.value = ''
-    let input1 = document.getElementById('d3');
-}
-
-function make2() {
-    let input = document.getElementById('d2');
-    let liElement = document.createElement('li');
-    let addElement = document.getElementById('li2');
-    liElement.innerText = input.value;
-    addElement.append(liElement);
-    let input1 = document.getElementById('d4');
-    let liElement2 = document.createElement('li');
-    let addElement3 = document.getElementById('li2');
-    addElement3.append(liElement2);
-    input1.value = ''
-}
-
-let i = input
+// function make2() {
+//     let input = document.getElementById('d2');
+//     let liElement = document.createElement('li');
+//     let addElement = document.getElementById('li2');
+//     liElement.innerText = input.value;
+//     addElement.append(liElement);
+//     let input1 = document.getElementById('d4');
+//     let liElement2 = document.createElement('li');
+//     let addElement3 = document.getElementById('li2');
+//     addElement3.append(liElement2);
+//     input1.value = ''
+// }
 
 function ler() {
-    let input = document.getElementById('d3');
-    let liElement = document.createElement('li');
-    let addElement = document.getElementById('li1');
-    let i = input.innerText * input1.innerText
-    addElement.append(liElement);
-    input.value = i
-    // input.value = ''
+    let input = document.getElementById('d1');
+    let input1 = document.getElementById('d3');
+    let addElement2 = document.getElementById('li1');
+
+
+
+    for (let i = 0; i < 5; i++) {
+        let liElement2 = document.createElement('li');
+        liElement2.innerText = input.value;
+        addElement2.append(liElement2);
+    }
+
+    input.value = ''
+
 }
 
